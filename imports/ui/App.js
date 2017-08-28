15e248d01cb321ed566d58c38e1d19eaae6d2b28
Wasupon Angsupokai <wasupon.a@scale360solutions.com>
2017-08-28 18:22:20 +0700

    add todo list

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,15 +1,41 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
 import { createContainer } from 'meteor/react-meteor-data'
+import { PropTypes } from 'prop-types'
 
 import { Tasks } from '../api/tasks.js'
 import { Task } from './Task'
 
 // App component - represents the whole app
 class App extends Component {
+
+  static propType = {
+    tasks: PropTypes.array.isRequired,
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      input: '',
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
   renderTasks() {
     return this.props.tasks.map((task) => (
       <Task key={task._id} task={task} />
     ))
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+
+    Tasks.insert({
+      text: this.refs.textInput.value,
+      createdAt: new Date(), // current time
+    })
+    
+    // Clear form
+    this.refs.textInput.value = ''
   }
 
   render() {
@@ -17,6 +43,14 @@ class App extends Component {
       <div className="container">
         <header>
           <h1>Todo List</h1>
+          <form className="new-task" onSubmit={this.handleSubmit} >
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Type to add new tasks"
+              defaultValue={this.state.input}
+            />
+          </form>
         </header>
 
         <ul>
@@ -27,12 +61,9 @@ class App extends Component {
   }
 }
 
-App.propTypes = {
-  tasks: PropTypes.array.isRequired,
-}
- 
+
 export default createContainer(() => {
   return {
-    tasks: Tasks.find({}).fetch(),
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
   }
 }, App)
--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -1,7 +1,17 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import { PropTypes } from 'prop-types'
 
 // Task component - represents a single todo item
-export const Task = (props) => <li>{props.task.text}</li>
+export const Task = (props) => {
+  const { text, createdAt } = props.task
+  const date = createdAt.toString().substr(0,10)
+  return(
+    <li>
+      <span> {text} </span>
+      <span className='date'>{date}</span>
+    </li>
+  )
+} 
 
 
 Task.propTypes = {
